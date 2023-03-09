d1bc6c5ca85da6b94a00f6c3e235ee554b26a040
seongwoocho-happy <progress0407@happytechnology.kr>
2023-03-09 17:55:07 +0900

    multi-purpose 모듈 생성

---
+++ multi-purpose/src/main/java/team/share/MultiPurposeApp.java
@@ -0,0 +1,12 @@
+package team.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiPurposeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiPurposeApp.class, args);
+    }
+}
